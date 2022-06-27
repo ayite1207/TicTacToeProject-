3cff22ccaedcf9864c9a45793f6c3565c2275c45
ayité <ayite.folly@gmail.com>
2022-06-27 21:54:53 +0200

    change ci file

---
+++ TicTacToe/Model/RulesManager.swift
@@ -0,0 +1,14 @@
+//
+//  RulesManager.swift
+//  TicTacToe
+//
+//  Created by ayite on 27/06/2022.
+//
+
+import Foundation
+
+class RulesManager {
+    func add(numberOne: Int, numberTwo: Int) -> Int {
+        return numberOne + numberTwo
+    }
+}
